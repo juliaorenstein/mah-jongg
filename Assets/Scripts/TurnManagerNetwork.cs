a88cc048065d8b4fdf5efddf465d8dc06af20fa6
juliaorenstein <juliaorenstein@gmail.com>
2023-11-04 11:42:35 +0000

    blah

--- Assets/Scripts/TurnManagerNetwork.cs
+++ Assets/Scripts/TurnManagerNetwork.cs
@@ -15,12 +15,13 @@ public class TurnManagerNetwork : NetworkBehaviour
     private void Awake()
     {
         GManager = GetComponent<GameManager>();
-        TManager = GetComponent<TurnManager>()
+        TManager = GetComponent<TurnManager>();
         TurnPlayerID = GManager.Dealer;
 
     }
 
     [Rpc(RpcSources.All, RpcTargets.All)]
     public void RPC_ShowTileInDiscard(int tileID)
-    { ShowTileInDiscard(GManager.TileList[tileID].transform.parent); }
+    { //ShowTileInDiscard(GManager.TileList[tileID].transform.parent);
+    }
 }
